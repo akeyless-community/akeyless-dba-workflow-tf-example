f67920ca5790a4edc06fe161dde0c1575de60484
Chris Gruel <chris.g@akeyless.io>
2022-03-03 01:29:32 -0500

    add analytics

--- main.tf
+++ main.tf
@@ -42,53 +42,53 @@ provider "akeyless" {
   }
 }
 
-# module "mongodb_atlas_producer" {
-#   source = "./producer"
-#   dyn_secret_config = {
-#     # The name of the dynamic secret
-#     resourceName = "/Azure MongoDB Atlas - Sample Analytics"
-#     # The name of the target database that the dynamic secret will be applied to
-#     targetName = "/Azure Atlas"
-#     # The time to live for the temporary credentials produced by the dynamic secret
-#     user_ttl = "8h"
-#     # The roles that the dynamic secret will have access to
-#     mongodb_roles = [
-#       {
-#         roleName     = "readWrite",
-#         databaseName = "sample_analytics"
-#       }
-#     ]
-#     # The name of the role that will grant the team access to the resource
-#     roleName = "/terraform-db/Team1"
-#     auth_methods = {
-#       # The key is the full path to the kubernetes auth method
-#       "/my-k8s-auth-method" = {
-#         subClaims = {
-#           namespace = "team"
-#         }
-#       }
-#       # The key is the full path to the SAML provider so the team members can access the secret
-#       "/OktaSAML" = {
-#         subClaims = {
-#           groups = "Team1"
-#         }
-#       }
-#     }
-#     ruleList = {
-#       # The key is the access path to the secret object
-#       "/k8s/*" = {
-#         # What permissions do you want the team to have
-#         allowedCapabilities = ["read", "list"]
-#         # The type of rule (eg. item-rule, target-rule, role-rule, auth-method-rule)
-#         ruleType = "item-rule"
-#       }
-#       # The key is the access path to the secret object
-#       "/Azure MongoDB Atlas - Sample Analytics" = {
-#         # What permissions do you want the team to have
-#         allowedCapabilities = ["read", "list"]
-#         # The type of rule (eg. item-rule, target-rule, role-rule, auth-method-rule)
-#         ruleType = "item-rule"
-#       }
-#     }
-#   }
-# }
+module "mongodb_atlas_producer" {
+  source = "./producer"
+  dyn_secret_config = {
+    # The name of the dynamic secret
+    resourceName = "/Azure MongoDB Atlas - Sample Analytics"
+    # The name of the target database that the dynamic secret will be applied to
+    targetName = "/Azure Atlas"
+    # The time to live for the temporary credentials produced by the dynamic secret
+    user_ttl = "8h"
+    # The roles that the dynamic secret will have access to
+    mongodb_roles = [
+      {
+        roleName     = "readWrite",
+        databaseName = "sample_analytics"
+      }
+    ]
+    # The name of the role that will grant the team access to the resource
+    roleName = "/terraform-db/Team1"
+    auth_methods = {
+      # The key is the full path to the kubernetes auth method
+      "/my-k8s-auth-method" = {
+        subClaims = {
+          namespace = "team1"
+        }
+      }
+      # The key is the full path to the SAML provider so the team members can access the secret
+      "/OktaSAML" = {
+        subClaims = {
+          groups = "Team1"
+        }
+      }
+    }
+    ruleList = {
+      # The key is the access path to the secret object
+      "/k8s/*" = {
+        # What permissions do you want the team to have
+        allowedCapabilities = ["read", "list"]
+        # The type of rule (eg. item-rule, target-rule, role-rule, auth-method-rule)
+        ruleType = "item-rule"
+      }
+      # The key is the access path to the secret object
+      "/Azure MongoDB Atlas - Sample Analytics" = {
+        # What permissions do you want the team to have
+        allowedCapabilities = ["read", "list"]
+        # The type of rule (eg. item-rule, target-rule, role-rule, auth-method-rule)
+        ruleType = "item-rule"
+      }
+    }
+  }
+}
